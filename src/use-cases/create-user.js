74b94e237541211bfaae6b3ab8728a1febe4eddb
sergiorbotelho <sergiorbotelho@gmail.com>
2024-05-18 11:33:51 -0300

    feat: add CreateUserCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
+
+import bcrypt from "bcrypt";
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user";
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    const userId = uuidv4();
+
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+
+    const createUser = await postgresCreateUserRepository.execute(user);
+
+    return createUser;
+  }
+}
