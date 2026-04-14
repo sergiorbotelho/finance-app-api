944692147624d1f86564b41c917ce74c5ba2945a
Sergio Botelho <sergiorbotelho@gmail.com>
2026-04-13 22:51:23 -0300

    feat: add requiredFieldsIsMissingResponse function

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -6,6 +6,7 @@ import {
   created,
   emailIsAlreadyInUseResponse,
   invalidPasswordResponse,
+  requiredFieldIsMissingResponse,
   serverError,
   validateRequiredFields,
 } from "../helpers/index.js";
@@ -23,9 +24,7 @@ export class CreateUserController {
         validateRequiredFields(params, requiredFields);
 
       if (!requiredFieldsWereProvided) {
-        return badRequest({
-          message: `The field ${missingField} is required.`,
-        });
+        return requiredFieldIsMissingResponse(missingField);
       }
 
       const passwordIsValid = checkIfPasswordIsValid(params.password);
