f8c045928bb2aa10a0fd017131553f1c528237bd
Sergio Botelho <sergiorbotelho@gmail.com>
2026-04-23 21:47:38 -0300

    fix: pass correct params to PostgresUpdateTransactionRepository

--- src/use-cases/transaction/update-transaction.js
+++ src/use-cases/transaction/update-transaction.js
@@ -1,17 +1,13 @@
-import { UserNotFoundError } from "../../errors/user.js";
-
 export class UpdateTransactionUseCase {
-  constructor(updateTransactionRepository, getTransactionByIdRepository) {
+  constructor(updateTransactionRepository) {
     this.updateTransactionRepository = updateTransactionRepository;
-    this.getTransactionByIdRepository = getTransactionByIdRepository;
   }
 
-  async execute(params) {
-    const user = await this.getUserByIdRepository.execute(params.userId);
-    if (!user) {
-      throw UserNotFoundError(params.userId);
-    }
-    const transaction = await this.updateTransactionRepository.execute(params);
+  async execute(transactionId, params) {
+    const transaction = await this.updateTransactionRepository.execute({
+      transactionId,
+      params,
+    });
     return transaction;
   }
 }
