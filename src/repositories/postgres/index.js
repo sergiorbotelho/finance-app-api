9296f0bc1ff19bf049784ebd47b5cfeced5d9945
Sergio Botelho <sergiorbotelho@gmail.com>
2026-05-01 20:58:33 -0300

    feat: add PostgresGetUserBalanceRepository

--- src/repositories/postgres/index.js
+++ src/repositories/postgres/index.js
@@ -4,6 +4,7 @@ export * from "./transaction/get-transactions-by-user-id.js";
 export * from "./transaction/update-transaction.js";
 export * from "./user/create-user.js";
 export * from "./user/delete-user.js";
+export * from "./user/get-user-balance.js";
 export * from "./user/get-user-by-email.js";
 export * from "./user/get-user-by-id.js";
 export * from "./user/update-user.js";
