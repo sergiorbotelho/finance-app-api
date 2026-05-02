f3f2fc7dbdfa53ee41f766e483a5dfab3973bffc
Sergio Botelho <sergiorbotelho@gmail.com>
2026-05-01 22:44:59 -0300

    feat: update execMigrations script to read all sql files

--- src/db/postgres/migrations/exec.js
+++ src/db/postgres/migrations/exec.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import fs from "fs";
-import { pool } from "../helper.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import { pool } from "../helper.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,10 +11,17 @@ const execMigration = async () => {
   const client = await pool.connect();
 
   try {
-    const filePath = path.join(__dirname, "01-init.sql");
-    const script = fs.readFileSync(filePath, "utf-8");
+    const files = fs
+      .readdirSync(__dirname)
+      .filter((file) => file.endsWith(".sql"));
 
-    await client.query(script);
+    for (const file of files) {
+      const filePath = path.join(__dirname, file);
+      const script = fs.readFileSync(filePath, "utf-8");
+
+      await client.query(script);
+      console.log(`Migration for file ${file} executed successfully.`);
+    }
 
     console.log("Migration executed successfully.");
   } catch (error) {
