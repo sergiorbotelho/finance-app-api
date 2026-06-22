8eb8ec2111f9833e52a4e1ae171030a49934653c
Sergio Botelho <sergiorbotelho@gmail.com>
2026-06-21 21:32:22 -0300

    test: ensure 400 is returned if password is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -89,4 +89,20 @@ describe("Create User Controller", () => {
 
     expect(result.statusCode).toBe(400);
   });
+  it("should return 400 if password is not provided", async () => {
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: "Sergio",
+        last_name: "Botelho",
+        email: "sergio",
+      },
+    };
+
+    const result = await createUserController.execute(httpRequest);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
