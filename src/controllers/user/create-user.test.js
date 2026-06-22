e3734405ec304ef66de795d7b2856a3a120948f3
Sergio Botelho <sergiorbotelho@gmail.com>
2026-06-21 21:00:32 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -24,4 +24,20 @@ describe("Create User Controller", () => {
     expect(result.statusCode).toBe(201);
     expect(result.body).toBe(httpRequest.body);
   });
+  it("should return 400 if first_name is not provided", async () => {
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        last_name: "Botelho",
+        email: "sergio@teste.com",
+        password: "12345678",
+      },
+    };
+
+    const result = await createUserController.execute(httpRequest);
+
+    expect(result.statusCode).toBe(400);
+  });
 });
