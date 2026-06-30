39aa1f4e3b3dd29745464ced8633c4fcc8f7657a
Sergio Botelho <sergiorbotelho@gmail.com>
2026-06-30 19:42:27 -0300

    test: ensure CreateUserUseCase is called with correct params

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -22,7 +22,7 @@ describe("Create User Controller", () => {
     const result = await createUserController.execute(httpRequest);
 
     expect(result.statusCode).toBe(201);
-    expect(result.body).toBe(httpRequest.body);
+    expect(result.body).toEqual(httpRequest.body);
   });
   it("should return 400 if first_name is not provided", async () => {
     const createUserUseCase = new CreateUserUseCaseStub();
@@ -105,5 +105,24 @@ describe("Create User Controller", () => {
     const result = await createUserController.execute(httpRequest);
 
     expect(result.statusCode).toBe(400);
+  });
+
+  it("Should call CreateUserUseCase with correct params", async () => {
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+    const httpRequest = {
+      body: {
+        first_name: "Sergio",
+        last_name: "Botelho",
+        email: "sergio@teste.com",
+        password: "12345678",
+      },
+    };
+
+    const executeSpy = jest.spyOn(createUserUseCase, "execute");
+
+    await createUserController.execute(httpRequest);
+
+    expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 });
